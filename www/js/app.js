32197130ffa753ef4d5db85dbbc5e3469811fc53
Sean McGrath <Sean McGrath>
2016-02-16 22:27:16 +0000

    Added a time delay functionality and a fading in and out.

--- www/js/app.js
+++ www/js/app.js
@@ -14,7 +14,7 @@ app.run(function ($ionicPlatform) {
 });
 
 
-app.controller('PredictionController', function ($scope) {
+app.controller('PredictionController', function ($scope, $timeout) {
 
 	var predictionList = [
 		"Signs point to yes",
@@ -40,7 +40,15 @@ app.controller('PredictionController', function ($scope) {
 	];
 
 	$scope.prediction = "Tap 8ball for an answer";
+    $scope.answered = true;
+    
     $scope.ask = function () {
-        $scope.prediction = predictionList[Math.floor(Math.random() * predictionList.length)];
+        $scope.answered = false;
+        $scope.prediction = "Asking the Oracle";
+        
+        $timeout(function() {
+            $scope.prediction = predictionList[Math.floor(Math.random() * predictionList.length)];
+            $scope.answered = true;
+        }, 2000 ); 
     };
 });
